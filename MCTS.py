fd15152bf9397d8e1bcd6d89991a2be2425ffb54
Youinhoooo <1372383120@qq.com>
2023-11-27 16:48:19 +0800

    add MCTS.py

--- MCTS.py
+++ MCTS.py
@@ -71,8 +71,110 @@ class TreeNode(object):
     def is_root(self):
         return self._parent is None
 
+# 蒙特卡洛搜索树
+class MCTS(object):
+    def __init__(self,policy_value_fn,c_puct=5,n_playout=2000):
+        '''policy_value_fn:接收board的盘面状态，返回落子概率和盘面评估得分'''
+        self._root = TreeNode(None,1.0)
+        self._policy = policy_value_fn
+        self._c_puct = c_puct
+        self.n_playout = n_playout
 
+    def _playout(self,state):
+        '''
+        进行一次搜索，根据叶节点的评估值进行反向更新树节点的参数
+        '''
+        node = self._root
+        while True:
+            if node.is_leaf():
+                break
+            # 贪心算法选择下一步行动
+            action,node = node.select(self._c_puct)
+            state.do_move(action)
 
+        # 使用网络评估叶子节点，网络输出（动作，概率）元组p的列表以及当前玩家视角的得分[-1,1]
+        action_probs, leaf_value = self._policy(state)
+        # 查看游戏是否结束
+        end, winner = state.game_end()
+        if not end:
+            node.expand(action_probs)
+        else:
+            # 对于结束状态，将叶子节点的值换为1或-1
+            if winner == -1:
+                leaf_value = 0.0
+            else:
+                leaf_value = (1.0 if winner == state.get_current_player_id() else -1.0)
 
+        # 在本次遍历中更新节点的值和访问次数
+        # 递归赋值
+        node.update_recursive(-leaf_value)
 
+    def get_move_probs(self,state,temp=1e-3):
+        '''
+        按顺序运行所有搜索并返回可用的动作及其相应的概率
+        state:当前游戏的状态
+        temp:介于(0,1]之间的温度参数
+        '''
+        for n in range(self.n_playout):
+            state_copy = copy.deepcopy(state)
+            self._playout(state_copy)
 
+        # 根据根节点处的访问计数来计算移动概率
+        act_visits = [(act,node._n_visits) for act,node in self._root._childern.items()]
+        acts,visits = zip(*act_visits)
+        act_probs = softmax(1.0 / temp * np.log(np.array(visits)+1e-10))
+        return acts,act_probs
+
+    def update_with_move(self,last_move):
+        '''
+        在当前的树上向前一步
+        '''
+        if last_move in self._root._children:
+            self._root = self._root._children[last_move]
+            self._root._parent = None
+
+        else:
+            self._root = TreeNode(None,1.0)
+
+    def __str__(self):
+        return 'MCTS'
+# 基于MCTS的AI玩家
+class MCTSPlayer(object):
+    def __init__(self,policy_value_function,c_puct=5,n_playout=2000,is_selfplay=0):
+        self.mcts = MCTS(policy_value_function,c_puct,n_playout)
+        self._is_selfplay = is_selfplay
+        self.agent = "AI"
+
+    def set_player_ind(self,p):
+        self.player = p
+
+    # 重置搜索树
+    def reset_player(self):
+        self.mcts.update_with_move(-1)
+
+    def __str__(self):
+        return 'MCTS {}'.format(self.player)
+
+    # 得到行动
+    def get_action(self,board,temp=1e-3,return_prob=0):
+        move_probs = np.zeros(2086)
+
+        acts, probs = self.mcts.get_move_probs(board,temp)
+        move_probs[list(acts)] = probs
+        if self._is_selfplay:
+            # 添加Dirichlet Noise进行搜索
+            move = np.random.choice(
+                acts,
+                p=0.75*probs+0.25*np.random.dirichlet(CONFIG['dirichlet']*np.ones(len(probs)))
+            )
+            # 更新根节点并重用搜索树
+            self.mcts.update_with_move(move)
+        else:
+            # 使用默认的temp=1e-3，它几乎相当于选择具有最高概率的移动
+            move = np.random.choice(acts,p=probs)
+            # 重置根节点
+            self.mcts.update_with_move(-1)
+        if return_prob:
+            return move.move_probs
+        else:
+            return move
